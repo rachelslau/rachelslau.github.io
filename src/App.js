acd374fd135bccf35d16a4db74636cd422c3ebca
Rachel Lau <rachellau@berkeley.edu>
2020-01-05 12:20:45 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Current from './pages/current/Current.js';
+import About from './pages/about/About.js';
+import Projects from './pages/projects/Projects.js';
+import Photos from './pages/photos/Photos.js';
+
+import './Overall.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact path='/' component={Current} />
+        <Route path='/about' component={About} />
+        <Route path='/projects' component={Projects} />
+        <Route path='/photo' component={Photos} />
+      </Switch>
+    );
+  }
+}
+
+export default App;
