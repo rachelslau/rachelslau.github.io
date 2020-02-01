d64ce2626671d32860f3ddb93d8be02ba1e6febb
Rachel Lau <rachellau@berkeley.edu>
2020-02-01 10:59:15 -0800

    Updates

---
+++ precache-manifest.adc1c637c6c24790f3ceb44d736ccce8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80065ebeeda23cd0fbefcbbd8ddfe603",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09bd8d2cc3c4539ff27a",
+    "url": "/static/css/main.42f6dd53.chunk.css"
+  },
+  {
+    "revision": "cb5992718effdcdb76e2",
+    "url": "/static/js/2.5691c6e9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.5691c6e9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "09bd8d2cc3c4539ff27a",
+    "url": "/static/js/main.b064c251.chunk.js"
+  },
+  {
+    "revision": "d42a7a6354aad52377e1",
+    "url": "/static/js/runtime-main.726767f6.js"
+  },
+  {
+    "revision": "6e8185bbed47e645fe49140d8b188dd8",
+    "url": "/static/media/profile.6e8185bb.JPG"
+  }
+]);
