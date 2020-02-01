0952cf70721b9c04b36be196ca8d3c0419e8e7a3
Rachel Lau <rachellau@berkeley.edu>
2020-02-01 10:57:31 -0800

    changing the package.json

--- src/pages/about/Hobbies.js
+++ src/pages/about/Hobbies.js
@@ -16,7 +16,14 @@ class Hobbies extends Component {
                             <Link className="subpage-link" to="/about-experience">experience</Link>
                             <Link className="subpage-link selected-subpage-link">hobbies</Link>
                         </div>
-
+                        <div>
+                            <Hobby
+                                title="A C A I"
+                                // picture=
+                                caption="acai"
+                                description="previously only a boba addict, this semester I have decided to use acai bowls as the go to food of choice when I want to hang out with someone."
+                                />
+                        </div>
                     </span>
                 </div>
             </div>
